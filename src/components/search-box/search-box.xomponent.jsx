915c899560b216b906645f8055a77363c1e0dd1f
Nenko Ivanov <nenko.iv4nov@gmail.com>
2023-07-21 19:27:09 +0300

    initial commit complete project

---
+++ src/components/search-box/search-box.xomponent.jsx
@@ -0,0 +1,11 @@
+import "./search-box.styles.css";
+
+const SearchBox = ({ onChangeHandler, placeholder, className }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+export default SearchBox;
