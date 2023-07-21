51a5ebe4c7697538a519737ee3e02e335fce28a7
Nenko Ivanov <nenko.iv4nov@gmail.com>
2023-07-21 19:35:04 +0300

    trying out

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
-  //humm
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
